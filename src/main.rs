fbfe91714fa069f4c23b020786899727013fe808
pprog <29218920+pprogrammingg@users.noreply.github.com>
2024-03-23 13:05:02 -0400

    leetcode max length solution

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::{env, process};
 
 use practice_rust::leetcode::array::max_non_decreasing_length::solution::find_maximum_length;
 
-
 // use guessing_game::guessing_game;
 // use hempcrete_community::build_hempcrete_community;
 // use mpsc_example::send_from_main_receive_in_thread;
@@ -11,7 +10,6 @@ use practice_rust::leetcode::array::max_non_decreasing_length::solution::find_ma
 // use multi_threaded_scope::{test_thread_variable_simple, test_thread_variable_with_scope};
 // use http_reqwest_example::reqwest_example;
 use practice_rust::leetcode::*;
-
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -53,7 +51,7 @@ fn main() {
     let nums = vec![2, 6, 1, 1, 3];
     assert!(find_maximum_length(nums) == 2, "test 1 failed!");
 
-    let nums = vec![5,4,3,2,1];
+    let nums = vec![5, 4, 3, 2, 1];
     assert!(find_maximum_length(nums) == 2, "test 2 failed!");
 
     let nums = vec![100, 5, 4, 3, 2];
