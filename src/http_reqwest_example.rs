ab2cd0e626b1f800aa2630515f3fa164cff4808a
pprog <29218920+pprogrammingg@users.noreply.github.com>
2024-03-23 13:03:51 -0400

    Reqwest samples

--- src/http_reqwest_example.rs
+++ src/http_reqwest_example.rs
@@ -1,4 +1,25 @@
-fn reqwest_example(){
+use reqwest::blocking::{Client, ClientBuilder};
 
+pub fn reqwest_example(){
+
+    let http_client = Client::new();
+    let http_result = http_client.get("https://www.trevorsullivan.net/").send();
+
+    if http_result.is_ok() {
+        println!("resuult is {:#?}", http_result.ok().unwrap().text().unwrap());
+    }
+    else if http_result.is_err() {
+        println!("error encountered, error: {:#?}", http_result.err());
+    }
+
+    let post_result = http_client.post("http://localhost:3000/submit")
+        .header("Authorization", "xxx")
+        .body("\"name\": \"pprog\"").send();
     
+    if post_result.is_ok(){
+        println!("post result is {:#?}", post_result.ok().unwrap().text().unwrap());
+    }
+    else if post_result.is_err() {
+        println!("error encountered, error: {:#?}", post_result.err().unwrap());
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ mod multi_threaded_scope;
 mod multi_threaded_mutex;
 mod mpsc_example;
 mod hempcrete_community;
+mod http_reqwest_example;
 
-use hempcrete_community::build_hempcrete_community;
-use mpsc_example::send_from_main_receive_in_thread;
-use multi_threaded_mutex::mutex_exmaple;
+
+// use hempcrete_community::build_hempcrete_community;
+// use mpsc_example::send_from_main_receive_in_thread;
+// use multi_threaded_mutex::mutex_exmaple;
 // use multi_threaded_basic::multi_threaded_example;
-//use multi_threaded_scope::{test_thread_variable_simple, test_thread_variable_with_scope};
-
+// use multi_threaded_scope::{test_thread_variable_simple, test_thread_variable_with_scope};
+use http_reqwest_example::reqwest_example;
 fn main() {
     // Run the traits tutorial example
     //run_traits_example();
@@ -24,5 +26,7 @@ fn main() {
 
    // send_from_main_receive_in_thread();
 
-   build_hempcrete_community();
+   //  build_hempcrete_community();
+
+   reqwest_example();
 }
